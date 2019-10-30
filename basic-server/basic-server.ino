17238dec5bf2174435ad0924021abc4b06c45728
Brenden Roland <bmr549@lindenwood.edu>
2019-10-30 11:56:14 -0500

    Moved processClientData into Loop
    I am getting the GET line at this point, but I still need to pull off the unneeded HTTP information and slashes.

--- basic-server/basic-server.ino
+++ basic-server/basic-server.ino
@@ -30,39 +30,54 @@ void setup() {
  * Loop
  */
 void loop() {
+  
   WiFiClient nextClient; // clients will connect to the server
   nextClient = myServer.available(); // checks for a new client
-  if (nextClient != NULL) {
+  
+  if (nextClient != NULL) { // another client is available
+    
     String clientData = ""; // no client data yet
     while (nextClient.connected()) {
-      if (nextClient.available()) { // has the client sent data?
-        processClientData(nextClient, clientData);
-      }
-    }
-  }
-}
-
-/*
- * Process Client Data
- */
-void processClientData(WiFiClient nextClient, String clientData) {
-  char ch = nextClient.read(); // read next character from client
-  if (ch == '\n') { // we have reached the end of a line
-    if (clientData.length() == 0 ) { // tell http client we are done
-      nextClient.println("HTTP/1.1 200 OK");
-      nextClient.println("Content-type:text/html");
-      nextClient.println("Connection: close");
-      nextClient.println();
-      nextClient.stop(); // disconnect from the client
-      Serial.println("Client Disconnected");
-    } else {
       
-    }
-    clientData = ""; // clear out current command (we're done with it)
-  } else if (ch != '\r') {
-    clientData += ch; // append character to data client sent
-  }
-}
+      if (nextClient.available()) { // has the client sent data?
+        char ch = nextClient.read(); // read next character from client
+        
+        if (ch == '\n') { // we have reached the end of a line
+          String clientRequest = ""; // client request after the GET command
+          if (clientData.length() == 0 ) { // tell http client we are done
+            nextClient.println("HTTP/1.1 200 OK");
+            nextClient.println("Content-type:text/html");
+            nextClient.println("Connection: close");
+            nextClient.println();
+            nextClient.stop(); // disconnect from the client
+            Serial.println("Client Disconnected");
+          } else {
+
+            // put one line of the request into clientRequest
+            clientRequest += clientData;
+
+            // check the line to see if it starts with the string "GET",
+            // if it does, then this will be the line with our special
+            // values that we added to the url, such as "value=0" put
+            // after the url in "192.168.x.x/value=0"
+            if (clientRequest.startsWith("GET")) {
+              Serial.println("Command Type: GET");
+              Serial.println(clientRequest.substring(3));
+            }
+            
+          }
+          clientData = ""; // clear out current command (we're done with it)
+        } else if (ch != '\r') {
+          clientData += ch; // append character to data client sent
+        } // end if (ch != '\r')
+        
+      } // end if (nextClient.available())
+      
+    } // end while
+    
+  } // end if (nextClient != NULL)
+  
+} // end loop()
 
 /*
  * Connect to WiFi
